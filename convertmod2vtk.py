23aa5c5dd951d34763d776089e1b16004d643d24
Darius Arnold <mail@dariusarnold.de>
2018-08-24 20:45:04 +0200

    Move conversion to new function

--- convertmod2vtk.py
+++ convertmod2vtk.py
@@ -34,120 +34,126 @@ def convert(startpoint, endpoint, numpoints, spacing):
 
     return int_coord
 
+def convertmod2vtk(out_file, inp_file, ohm_file=None):
 
-try:
-    out_file = sys_argv[1]
-    inp_file = sys_argv[2]
-    if len(sys_argv) > 3:
-        ohm_file = sys_argv[3]
-        topography = True
-    else:
-        topography = False
-except:
-    print("USAGE: \n  convertmod2vtk.py output_file input_file [ohm_file_for_topography] \n e.g. \n convertmod2vtk.py Wenner.vtk Wenner.mod Wenner.ohm\n\n")
-    sys_exit()
+    # only use topography if ohm file is given
+    topography = ohm_file is not None
 
-# read mod file
-inp = open(inp_file,'r')
-lines = inp.readlines()
-inp.close()
-del lines[0] # delete header line (#x1/m	x2/m	z1/m	z2/m	rho/Ohmm coverage)
-x = []
-z = []
-rho = []
-coverage = []
-for line in lines:
-    tmp = line.split()
-    # x holds x1 x2 pair of coordinates
-    x.append([float(tmp[0]),float(tmp[1])]) ###?
-    # z holds z1 z2 pair of coordinates
-    z.append([-float(tmp[2]),-float(tmp[3])])
-    # measured specific resistivity
-    rho.append(float(tmp[4]))
-    # TODO what is this parameter?
-    coverage.append(float(tmp[5]))
-
-# read ohm file
-if topography:
-    ohm = open(ohm_file,'r')
-    lines = ohm.readlines()
-    ohm.close()
-    index = lines.index('# x h for each topo point\r\n')
-    num_topopoints = int(lines[index-1].split('#')[0])
-    lines = lines[index+1:]
-    x_topo = []
-    z_topo = []
+    # read mod file
+    inp = open(inp_file,'r')
+    lines = inp.readlines()
+    inp.close()
+    del lines[0] # delete header line (#x1/m	x2/m	z1/m	z2/m	rho/Ohmm coverage)
+    x = []
+    z = []
+    rho = []
+    coverage = []
     for line in lines:
         tmp = line.split()
-        x_topo.append(float(tmp[0])) ###
-        z_topo.append(float(tmp[1])) ###
-    topo = dict(zip(x_topo,z_topo))
+        # x holds x1 x2 pair of coordinates
+        x.append([float(tmp[0]),float(tmp[1])]) ###?
+        # z holds z1 z2 pair of coordinates
+        z.append([-float(tmp[2]),-float(tmp[3])])
+        # measured specific resistivity
+        rho.append(float(tmp[4]))
+        # coverage is how often a block was targeted during measurement. Higher coverage -> more confident result
+        coverage.append(float(tmp[5]))
 
-# some calculations
-num_cells = len(rho)
-points = []
-cells = []
-for i in range(num_cells):
-    cell = []
-    for j in range(2):
-        for k in range(2):
-            point = [x[i][j], 0.0, z[i][k]]
+    # read ohm file
+    if topography:
+        ohm = open(ohm_file,'r')
+        lines = ohm.readlines()
+        ohm.close()
+        index = lines.index('# x h for each topo point\r\n')
+        num_topopoints = int(lines[index-1].split('#')[0])
+        lines = lines[index+1:]
+        x_topo = []
+        z_topo = []
+        for line in lines:
+            tmp = line.split()
+            x_topo.append(float(tmp[0])) ###
+            z_topo.append(float(tmp[1])) ###
+        topo = dict(zip(x_topo,z_topo))
 
-            try:
-                index = points.index(point)
-                cell.append(index)
-            except:
-                points.append(point)
-                cell.append(len(points)-1)
-    index = cell[2]
-    cell[2] = cell[3]
-    cell[3] = index
-    cells.append(cell)
-num_points = len(points)
+    # some calculations
+    num_cells = len(rho)
+    points = []
+    cells = []
+    for i in range(num_cells):
+        cell = []
+        for j in range(2):
+            for k in range(2):
+                point = [x[i][j], 0.0, z[i][k]]
+
+                try:
+                    index = points.index(point)
+                    cell.append(index)
+                except:
+                    points.append(point)
+                    cell.append(len(points)-1)
+        index = cell[2]
+        cell[2] = cell[3]
+        cell[3] = index
+        cells.append(cell)
+    num_points = len(points)
 
 
-if topography:
+    if topography:
+        for point in points:
+            if point[0] in topo:
+                point[2] += topo[point[0]]
+            else:
+                #use nearest topography point
+                point[2] += topo[min(topo.keys(), key=lambda k: abs(k-point[0]))]
+
+    startpoint = (356933, 5686395)
+    endpoint = (357127, 5686380)
+    numpoints = 50
+    spacing = 4
+
+    converted_points = convert(startpoint, endpoint, numpoints, spacing)
+
     for point in points:
-        if point[0] in topo:
-            point[2] += topo[point[0]]
+        point[0], point[1] = converted_points[point[0]]
+
+    # write VTK file
+    out = open(out_file,'w')
+    out.write('# vtk DataFile Version 3.0\n')
+    out.write(os.path.split(inp_file)[1]+'\n')
+    out.write('ASCII\n')
+
+    out.write('DATASET UNSTRUCTURED_GRID\n')
+    out.write('POINTS {0:d} float\n'.format(num_points))
+    for point in points:
+        out.write(' '.join(str(x) for x in point)+'\n')
+    out.write('CELLS {0:d} {1:d}\n'.format(num_cells,num_cells*5))
+    for cell in cells:
+        out.write('4 '+' '.join(str(i) for i in cell)+'\n')
+    out.write('CELL_TYPES {0:d}\n'.format(num_cells))
+    for cell in cells:
+        out.write('9\n')
+    out.write('\n')
+    out.write('CELL_DATA '+str(num_cells)+'\n')
+    out.write('SCALARS rho float 1\n')
+    out.write('LOOKUP_TABLE default\n')
+    for value in rho:
+        out.write('{0}\n'.format(value))
+    out.write('SCALARS coverage float 1\n')
+    out.write('LOOKUP_TABLE default\n')
+    for value in coverage:
+        out.write('{0}\n'.format(value))
+    out.close()
+
+if __name__ == '__main__':
+    try:
+        out_file = sys_argv[1]
+        inp_file = sys_argv[2]
+        if len(sys_argv) > 3:
+            ohm_file = sys_argv[3]
         else:
-            #use nearest topography point
-            point[2] += topo[min(topo.keys(), key=lambda k: abs(k-point[0]))]
+            ohm_file = None
+    except:
+        print("USAGE: \n  convertmod2vtk.py output_file input_file [ohm_file_for_topography] \n e.g. \n convertmod2vtk.py Wenner.vtk Wenner.mod Wenner.ohm\n\n")
+        sys_exit()
 
-startpoint = (357028, 5686437)
-endpoint = (357126, 5686411)
-numpoints = 50
-spacing = 2
-
-converted_points = convert(startpoint, endpoint, numpoints, spacing)
-
-for point in points:
-    point[0], point[1] = converted_points[point[0]]
-
-# write VTK file
-out = open(out_file,'w')
-out.write('# vtk DataFile Version 3.0\n')
-out.write(os.path.split(inp_file)[1]+'\n')
-out.write('ASCII\n')
-
-out.write('DATASET UNSTRUCTURED_GRID\n')
-out.write('POINTS {0:d} float\n'.format(num_points))
-for point in points:
-    out.write(' '.join(str(x) for x in point)+'\n')
-out.write('CELLS {0:d} {1:d}\n'.format(num_cells,num_cells*5))
-for cell in cells:
-    out.write('4 '+' '.join(str(i) for i in cell)+'\n')
-out.write('CELL_TYPES {0:d}\n'.format(num_cells))
-for cell in cells:
-    out.write('9\n')
-out.write('\n')
-out.write('CELL_DATA '+str(num_cells)+'\n')
-out.write('SCALARS rho float 1\n')
-out.write('LOOKUP_TABLE default\n')
-for value in rho:
-    out.write('{0}\n'.format(value))
-out.write('SCALARS coverage float 1\n')
-out.write('LOOKUP_TABLE default\n')
-for value in coverage:
-    out.write('{0}\n'.format(value))
-out.close()
+    convertmod2vtk(out_file, inp_file, ohm_file)
