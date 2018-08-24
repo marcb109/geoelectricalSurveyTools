29c77927f3a247835b81c5e0eab45fa57eccfb36
Darius Arnold <mail@dariusarnold.de>
2018-08-24 20:57:07 +0200

    Add very basic regression test
    Just compare the output of the previous version that is kept in a file with the result of the function when called with the same parameters

---
+++ test_convertmod2vtk.py
@@ -0,0 +1,21 @@
+import os
+import unittest
+from convertmod2vtk import convertmod2vtk
+import filecmp
+
+
+class TestWholeScript(unittest.TestCase):
+
+    def setUp(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.input_file_mod = os.path.join(dir_path, "Profil1.mod")
+        self.output_file_name = os.path.join(dir_path, "Profil1_Result.vtk")
+        self.output_file_correct_result = os.path.join(dir_path, "Profil1.vtk")
+
+    def test_convertmod2vtk(self):
+        # remove old results if they exist
+        try:
+            os.remove(self.output_file_name)
+        except FileNotFoundError: pass
+        convertmod2vtk(self.output_file_name, self.input_file_mod)
+        self.assertTrue(filecmp.cmp(self.output_file_correct_result, self.output_file_name), msg='Result of convertmod2vtk is different different from expected result!')
