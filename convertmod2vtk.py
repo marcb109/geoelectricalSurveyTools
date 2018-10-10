80984a63e0b10c17cb2d6c5c474f78ee5e87e0b2
Darius Arnold <mail@dariusarnold.de>
2018-10-10 22:27:40 +0200

    Improve argparser: print help w/o arguments, add example call

--- convertmod2vtk.py
+++ convertmod2vtk.py
@@ -117,15 +117,29 @@ def convertmod2vtk(out_file, inp_file, start_point, end_point, topo_file=None):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Converts .mod to .vtk file, adding surface geometry.")
+    epilog_string = """\
+    Example call: convertmod2vtk.py Profil1.vtk Prodil1.mod Profil1.ohm 378455 5701392 378455 5701392
+        This would call the script to read topography from the ohm file and use a 
+        start point of 378455 5701392 and an end point of 378455 5701392. Output is 
+        saved to the Profil1.vtk file."""
+
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description="Converts .mod to .vtk file, adding surface geometry.",
+                                     epilog=epilog_string)
     parser.add_argument("output_vtk", help="Filepath/filename of .vtk file in which the results are saved.")
     parser.add_argument("input_mod", help="Filepath/filename of .mod file from which inputs are read.")
     parser.add_argument("input_topo", help="Filepath/filename of dem model or ohm file used for elevation")
-    parser.add_argument("start_point", nargs=2, type=float, help="UTM north east coordinate of start point")
-    parser.add_argument("end_point", nargs=2, type=float, help="UTM north east coordinate of end point")
-    args = parser.parse_args()
+    parser.add_argument("start_point", nargs=2, type=float, help="UTM north east coordinate of start point of array")
+    parser.add_argument("end_point", nargs=2, type=float, help="UTM north east coordinate of end point of array")
+    try:
+        args = parser.parse_args()
+        convertmod2vtk(args.output_vtk, args.input_mod, args.start_point, args.end_point, args.input_topo)
+    except SystemExit as err:
+        # if no options were used, print help.
+        # Additional if check avoids double printing help whith -h flag
+        if err.code == 2: parser.print_help()
 
-    convertmod2vtk(args.output_vtk, args.input_mod, args.start_point, args.end_point, args.input_topo)
 
 if __name__ == '__main__':
     main()
